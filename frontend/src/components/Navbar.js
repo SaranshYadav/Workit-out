dec5c6b6c901a7a13f6d2b099bb21893072df192
Saransh <97433080+SaranshYadav@users.noreply.github.com>
2024-07-20 13:08:08 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout Buddy</h1>
+          <h1>Workit out</h1>
         </Link>
       </div>
     </header>
